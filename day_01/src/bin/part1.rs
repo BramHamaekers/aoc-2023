b45bd3f016324ade220cace278a15ba118fb5bac
BramHamaekers <bram.hamaekers@gmail.com>
2023-12-01 13:21:31 +0100

    day_1: complete

--- day_01/src/bin/part1.rs
+++ day_01/src/bin/part1.rs
@@ -1,11 +1,23 @@
 fn main() {
-    let input = include_str!("./input/input1.txt");
+    let input = include_str!("./input/input.txt");
     let output = part1(input);
     dbg!(output);
 }
 
+fn find_digit(line: &str) -> i32 {
+    line.chars()
+        .find(|c| c.is_ascii_digit())
+        .map(|c| c as i32 - 48)
+        .unwrap_or(0)
+}
+
 pub fn part1(input: &str) -> i32 {
-    return 0
+    let lines: Vec<String> = input.lines().map(String::from).collect();
+    let mut values = Vec::<i32>::new();
+    for line in lines {
+        values.push(find_digit(&line) * 10 + find_digit(&line.chars().rev().collect::<String>()));
+    }
+    return values.iter().sum();
 }
 
 #[cfg(test)]
@@ -14,8 +26,13 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        let expected_output = 0;
-        let result = part1("");
+        let expected_output = 142;
+        let result = part1(
+            "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet",
+        );
         assert_eq!(result, expected_output);
     }
 }
--- day_01/src/bin/part2.rs
+++ day_01/src/bin/part2.rs
@@ -1,11 +1,59 @@
+use std::collections::HashMap;
+
+fn create_key_map() -> HashMap<&'static str, i32> {
+    [
+        ("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4),
+        ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9),
+    ].iter().cloned().collect()
+}
+
 fn main() {
-    let input = include_str!("./input/input2.txt");
+    let input = include_str!("./input/input.txt");
     let output = part2(input);
     dbg!(output);
 }
 
+fn find_first_digit(line: &str) -> i32 {
+    let key_map: HashMap<&str, i32> = create_key_map();
+    let mut searched = String::new();
+    for char in line.chars() {
+        if char.is_ascii_digit() {
+            return char as i32 - 48;
+        }
+        searched.push(char);
+        for key in key_map.keys() {
+            if searched.contains(key) {
+                return key_map[key];
+            }
+        }
+    }
+    return 0;
+}
+
+fn find_last_digit(line: &str) -> i32 {
+    let key_map: HashMap<&str, i32> = create_key_map();
+    let mut searched = String::new();
+    for char in line.chars().rev() {
+        if char.is_ascii_digit() {
+            return char as i32 - 48;
+        }
+        searched.insert(0, char);
+        for key in key_map.keys() {
+            if searched.contains(key) {
+                return key_map[key];
+            }
+        }
+    }
+    return 0;
+}
+
 pub fn part2(input: &str) -> i32 {
-    return 0
+    let lines: Vec<String> = input.lines().map(String::from).collect();
+    let mut values = Vec::<i32>::new();
+    for line in lines {
+        values.push(find_first_digit(&line) * 10 + find_last_digit(&line));
+    }
+    return values.iter().sum();
 }
 
 #[cfg(test)]
@@ -14,8 +62,14 @@ mod tests {
 
     #[test]
     fn part2_test() {
-        let expected_output = 0;
-        let result = part2("");
+        let expected_output = 281;
+        let result = part2("two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen");
         assert_eq!(result, expected_output);
     }
 }
