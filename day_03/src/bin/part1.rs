dc5c1dff99b093cd6bdc1db2875584b20075afdd
BramHamaekers <bram.hamaekers@gmail.com>
2023-12-03 21:36:32 +0100

    day_03: part1 works

--- day_03/src/bin/part1.rs
+++ day_03/src/bin/part1.rs
@@ -4,9 +4,77 @@ fn main() {
     dbg!(output);
 }
 
+pub fn build_number (number: i32, digit: i32) -> i32 {
+    return (number * 10) + digit;
+}
+
+pub fn is_symbol(char: char) -> bool {
+    return char != '.' && !char.is_digit(10);
+}
+
+pub fn has_adjacent_symbol (matrix: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+        let directions = vec![
+        (-1, 0), (1, 0), (0, -1), (0, 1), // horizontal and vertical
+        (-1, -1), (-1, 1), (1, -1), (1, 1), // diagonal
+    ];
+    directions
+        .iter()
+        .any(|&(x, y)| {
+            let new_x = (i as i32 + x as i32) as usize;
+            let new_y = (j as i32 + y as i32) as usize;
+            new_x < matrix.len() && new_y < matrix[0].len() && is_symbol(matrix[new_x][new_y])
+        })
+    }
+
 pub fn part1(input: &str) -> i32 {
-    let lines: Vec<String> = input.lines().map(String::from).collect();
-    return 0;
+    let matrix: Vec<Vec<char>> = input
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let mut result = 0;
+
+    for (i, row) in matrix.iter().enumerate() {
+        let mut number = 0;
+        let mut building_number = false;
+        let mut is_part_number = false;
+
+        for (j, &element) in row.iter().enumerate() {
+            if element != '.' {
+                if element.is_digit(10) {
+                    if !building_number {
+                        building_number = true;
+                    }
+                    if !is_part_number {
+                        is_part_number = has_adjacent_symbol(&matrix, i, j);
+                    }
+                    number = build_number(number, element as i32 - '0' as i32);
+                }
+                else if building_number {
+                        if is_part_number {
+                            result += number;
+                        }
+                        number = 0;
+                        building_number = false;
+                        is_part_number = false;
+                    }
+            } else if building_number {
+                    if is_part_number {
+                        result += number;
+                    }
+                    number = 0;
+                    building_number = false;
+                    is_part_number = false;
+                }
+        }
+
+        if is_part_number {
+            result += number;
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -15,8 +83,27 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        let expected_output = 0;
-        let result = part1("",
+        let expected_output = 4361;
+        let result = part1(
+"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..",
+        );
+        assert_eq!(result, expected_output);
+    }
+
+    #[test]
+    fn part1_edge_case() {
+        let expected_output = 3;
+        let result = part1(
+"*1#2",
         );
         assert_eq!(result, expected_output);
     }
